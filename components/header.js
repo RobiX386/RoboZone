f03e368384bd1ef873fa2dba21ad3d722c2da89d
RaresZmf <64685406+RaresZmf@users.noreply.github.com>
2023-05-19 23:54:11 +0300

    Created components folder

---
+++ components/header.js
@@ -0,0 +1,11 @@
+export default function Headerr(){
+    return (
+        <div className="w-full flex justify-between items-center px-[100px] darkLightBackground">
+            <h1>RoboZone</h1>
+            <div className="flex justify-between items-center w-1/12">
+                <p className="text-lg ">About</p>
+                <p className="text-lg">Home</p>
+            </div>
+        </div>
+    )
+}
