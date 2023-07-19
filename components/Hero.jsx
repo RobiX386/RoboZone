ad30b7136ee8663ac733d7dbc8571b0b25995ea9
Robert Paun <105244110+RobiX386@users.noreply.github.com>
2023-07-19 04:52:21 +0300

    fixed bug

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 
-import Logo from './Logo';
-
 const Hero = () => (
   <div className="hero my-5 text-center" data-testid="hero">
-    <Logo testId="hero-logo" />
     <h1 className="mb-4" data-testid="hero-title">
       Next.js Sample Project
     </h1>
