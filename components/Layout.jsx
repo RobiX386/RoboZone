b06d77608ed9bafa29ab479f7dc45f51af742387
Robert Paun <105244110+RobiX386@users.noreply.github.com>
2023-07-19 05:47:04 +0300

    ultima incercare

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -4,7 +4,6 @@ import Head from 'next/head';
 import Loading from './Loading';
 import NavBar from './NavBar';
 import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0/client';
-import Footer from '../components/Footer';
 
 function Layout({ children }) {
   const [loading, setloading] = useState(true)
@@ -13,17 +12,21 @@ function Layout({ children }) {
       setloading(false);
     }, 2000);
   }, [])
-  
+
   return (
     <>
       {!loading ? <><Head>
         <title>RoboZone</title>
       </Head>
-      <main id="app" className='flex flex-col bgpattern w-screen h-screen justify-between text-black content-center items-center'>
-        <NavBar />
-        <Container>{children}</Container>
-        <Footer />
-      </main> </>: <Loading />}
+        <main id="app" className='flex flex-col bgpattern w-screen h-screen justify-between text-black content-center items-center'>
+          <NavBar />
+          <Container>{children}</Container>
+          <div className='shadow-md border text-sm text-opacity-40 text-black rounded-b-none border-b-0 p-5 flex flex-row justify-between bg-white rounded-xl w-[500px]'>
+            <span>Github</span>
+            <span>About</span>
+            <span>Copyright 2023 &copy; RoboZone</span>
+          </div>
+        </main> </> : <Loading />}
     </>
   )
 
